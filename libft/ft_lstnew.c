fe5b6146d120a20ab6c489bcbe44a87d96ebd94d
Arrow Eldridge <aeldridg@pr-k2.kzn.21-school.ru>
2021-05-03 19:19:38 +0300

    123

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -4,10 +4,10 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*the_new;
 
-	the_new = (t_list *)malloc(sizeof(content));
+	the_new = (t_list *)malloc(sizeof(t_list));
 	if (!the_new)
 		return (NULL);
-	the_new -> content = content;
-	the_new -> next = NULL;
+	the_new->content = content;
+	the_new->next = NULL;
 	return (the_new);
 }
