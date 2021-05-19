e18110567dfde582acf7cc77ec311191fe2b4cca
Arrow Eldridge <aeldridg@ge-a2.kzn.21-school.ru>
2021-05-19 16:26:32 +0300

    utils

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -34,26 +34,26 @@ char	*strjoin_free(char *s1, char *s2)
 {
 	size_t	l;
 	size_t	j;
-	size_t	s11;
-	size_t	s21;
-	char	*c;
+	size_t	s1_count;
+	size_t	s2_count;
+	char	*arr;
 
 	if (!s1 && !s2)
 		return (NULL);
 	l = -1;
 	j = 0;
-	s11 = ft_strlen(s1);
-	s21 = ft_strlen(s2);
-	c = (char *)malloc(sizeof(char) * (s11 + s21 + 1));
-	if (!c)
+	s1_count = ft_strlen(s1);
+	s2_count = ft_strlen(s2);
+	arr = (char *)malloc(sizeof(char) * (s1_count + s2_count + 1));
+	if (!arr)
 		return (NULL);
-	while (++l < s11)
-		c[l] = s1[l];
-	while (j < s21)
-		c[l++] = s2[j++];
-	c[l] = '\0';
+	while (++l < s1_count)
+		arr[l] = s1[l];
+	while (j < s2_count)
+		arr[l++] = s2[j++];
+	arr[l] = '\0';
 	free (s1);
-	return (c);
+	return (arr);
 }
 
 int	nl_check(char *str)
