44dd3a164c5094132f9cb02e737fd297eb19bd84
Arrow Eldridge <aeldridg@si-c1.kzn.21-school.ru>
2021-07-07 14:26:06 +0300

    exam

--- so_long/main/main.c
+++ so_long/main/main.c
@@ -1,6 +1,7 @@
 #include "../main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <mlx.h>
 
 // int	main(void)
 // {
@@ -49,8 +50,6 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 int	close(int keycode, t_data *img)
 {
     printf("Key is - %d\n", keycode);
-    // if ((mlx_mouse_show()))
-    //     printf("Hello!\n");
 	if (keycode == 53) {
         mlx_destroy_window(img->mlx, img->win);
         exit (0);
@@ -60,7 +59,14 @@ int	close(int keycode, t_data *img)
     return (0);
 }
 
-// loop_hook(void *param);
+int	close2(int keycode, t_data *img)
+{
+    if (keycode)
+        exit (0);
+    return (0);    
+}
+
+// int	render_next_frame(void *YourStruct)
 // {
 
 // }
@@ -71,48 +77,50 @@ int	main(void)
 	// void	*mlx.win;
 	t_data	img;
     int i = 0;
-    int i2 = 149;
+    int i2 = 999;
     int i3 = 150;
-    int d150 = 150;
+    int d150 = 1000;
+    int d1502 = 1000;
     int d0 = 0;
     int w = 500, h = 500;
     int tmp1, tmp2;
-    int color = 0x00FF0000;
+    int color = 0xFF00;
     char	*relative_path = "~/Downloads/check.xpm";
 
     // img.line_length = 5000;
     // img.bits_per_pixel = 300;
-    // img.endian = 1;
+    // img.endian = 0;
 
 	img.mlx = mlx_init();
-	img.win = mlx_new_window(img.mlx, 1920, 1080, "Hello world!");
-    img.img = mlx_new_image(img.mlx, 1920, 1080);
-	// if (!(img.img2 = mlx_png_file_to_image(img.mlx, relative_path, &img.w, &img.h))) {
+	img.win = mlx_new_window(img.mlx, 1920, 1080, "check");
+    img.img = mlx_new_image(img.mlx, 800, 600);
+    // if(!(img.img = mlx_xpm_file_to_image(img.mlx, relative_path, &img.w, &img.h))) {
     //     printf("error:(\n");
     //     exit (2);
     // }
-    mlx_png_file_to_image(img.mlx, "~/Downloads/check.xpm", &img.w, &img.h);
+    // mlx_png_file_to_image(img.mlx, "~/Downloads/check.xpm", &img.w, &img.h);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-    while (d0 < 150) 
-    {
-        tmp1 = i, tmp2 = i2;
-        while (i < 150) {                    
-            my_mlx_pixel_put(&img, i, d0, color);
-            my_mlx_pixel_put(&img, i++, d150, color);
-            my_mlx_pixel_put(&img, d0, i2, color);
-            my_mlx_pixel_put(&img, d150, i2--, color);
-        }
-        i = tmp1;
-        i2 = tmp2;
-        d0++;
-        d150--;
-    }
-    mlx_put_image_to_window(img.mlx, img.win, img.img, 500, 500);
+    my_mlx_pixel_put(&img, 5, 5, color);
+    // while (i < 1000) {                    
+    //     my_mlx_pixel_put(&img, i, 0, color);
+    //     my_mlx_pixel_put(&img, i++, 1000, color);
+    //     my_mlx_pixel_put(&img, 0, i2, color);
+    //     my_mlx_pixel_put(&img, 1000, i2--, color);
+    // }
+    // break ;
+        // color = 0xFF;
+        // i = tmp1;
+        // i2 = tmp2;
+        // d0++;
+        // d150--;
+        // d1502--;
+    mlx_put_image_to_window(img.mlx, img.win, img.img, 0, 0);
     // mlx_key_hook(mlx.win, key_hook, &img);
     mlx_hook(img.win, 2, 1L<<0, close, &img);
+    mlx_hook(img.win, 17, 0, close2, &img);
     // mlx_hook(img.win, 7, 1L<<4, close, &img);
     // mlx_mouse_hook (img.win, close, &img);
-    // mlx_loop_hook(img.mlx, loop_hook, &img);
+    // mlx_loop_hook(img.mlx, render_next_frame, img);
 	mlx_loop(img.mlx);
     return 0;
 }
